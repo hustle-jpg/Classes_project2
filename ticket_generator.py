39f3db604e6b160287f6d82e6a6099949ea4715f
Hustle <real4meal@gmail.com>
2024-10-13 19:11:27 +0500

    Added generator

--- ticket_generator.py
+++ ticket_generator.py
@@ -5,12 +5,17 @@ class TicketGenerator:
     def __init__(self, tickets):
         self.tickets = tickets
 
+    def ticket_generator(self):
+        for ticket in self.tickets:
+            yield ticket
+
     def to_csv(self, filename):
         with open(filename, 'w', newline='') as csvfile:
             fieldnames = ['text', 'extra_questions', 'signed_by_dean']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
-            for ticket in self.tickets:
+
+            for ticket in self.ticket_generator():
                 writer.writerow({
                     'text': ticket.text,
                     'extra_questions': ','.join(ticket.extra_questions),
@@ -19,4 +24,4 @@ class TicketGenerator:
 
     def to_json(self, filename):
         with open(filename, 'w') as jsonfile:
-            json.dump([ticket.__dict__ for ticket in self.tickets], jsonfile, indent=4)
+            json.dump([ticket.__dict__ for ticket in self.ticket_generator()], jsonfile, indent=4)
